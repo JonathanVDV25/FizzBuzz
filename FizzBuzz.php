578bef5440d460d8177615304dbee6dace8f922b
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-01-20 19:17:42 +0100

    modify - returns fizzbuzz when $number % 3 AND 5 == 0

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -8,6 +8,7 @@ class FizzBuzz
     {
         if($number % 3 == 0) return "fizz";
         elseif ($number % 5 == 0) return "buzz";
+        elseif($number % 3 == 0 && $number % 5 == 0) return "fizzbuzz";
         else return $number;
 
     }
