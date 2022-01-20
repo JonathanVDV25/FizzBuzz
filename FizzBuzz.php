e00fd0fa116c5c1e49c78e87c3b8320caa451030
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-01-20 18:29:35 +0100

    refactor - count returns number

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -6,8 +6,7 @@ class FizzBuzz
 
     public function count($number):string
     {
-        if($number == 1) return "1";
-        elseif ($number == 2) return "2";
+        return $number;
 
     }
 }
