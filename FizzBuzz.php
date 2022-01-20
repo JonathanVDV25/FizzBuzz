11970c6536b6d4646a2fb4d91af73d62f252016d
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-01-20 19:12:44 +0100

    modify - use of modulo in count instead of equality

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -6,8 +6,8 @@ class FizzBuzz
 
     public function count($number):string
     {
-        if($number == 3) return "fizz";
-        elseif ($number == 5) return "buzz";
+        if($number % 3 == 0) return "fizz";
+        elseif ($number % 5 == 0) return "buzz";
         else return $number;
 
     }
